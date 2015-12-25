74949bf9d0bda5064fccf4b2dd39ebf765938a31
hieutrantrung10 <hieutrantrung10@gmail.com>
2015-12-25 21:57:04 +0700

    Revert "Submit new Code"
    This reverts commit e274e062bd0eb588a9e5e31d8d96d1cb8e201bbf.

--- OnlineShopv2/OnlineShopv2/Areas/Admin/Controllers/ContentController.cs
+++ OnlineShopv2/OnlineShopv2/Areas/Admin/Controllers/ContentController.cs
@@ -1,6 +1,4 @@
-﻿using Model.Dao;
-using Model.EF;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,45 +13,9 @@ namespace OnlineShopv2.Areas.Admin.Controllers
         {
             return View();
         }
-        [HttpGet]
         public ActionResult Create()
         {
-            SetViewBag();
             return View();
-        }
-        [HttpGet]
-        public ActionResult Edit(long id)
-        {
-            var dao = new ContentDao();
-            var content = dao.GetByID(id);
-            SetViewBag(content.CategoryID);
-            return View();
-        }
-        [HttpPost]
-        [ValidateInput(false)]
-        public ActionResult Create(Content model)
-        {
-            if (ModelState.IsValid)
-            {
-
-            }
-            SetViewBag();
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Edit(Content model)
-        {
-            if (ModelState.IsValid)
-            {
-
-            }
-            SetViewBag(model.CategoryID);
-            return View();
-        }
-        public void SetViewBag(long ? selectedId = null)
-        {
-            var dao = new CategoryDao();
-            ViewBag.CategoryID = new SelectList(dao.ListAll(),"ID","Name",selectedId);
         }
     }
 }
